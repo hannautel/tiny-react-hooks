ae4ca8ae646ed6906ff0d57bfbc24d3567b9f765
Clement <tuananhit.oct@gmail.com>
2025-04-19 15:41:03 +0700

    fix: coverage should not analyze the index.ts file

--- packages/tiny-react-hooks/vitest.config.ts
+++ packages/tiny-react-hooks/vitest.config.ts
@@ -8,6 +8,8 @@ export default defineConfig({
     coverage: {
       provider: 'v8',
       extension: ['.ts'],
+      include: ['src/**/*.ts'],
+      exclude: ['src/**/index.ts']
     },
   },
 });
